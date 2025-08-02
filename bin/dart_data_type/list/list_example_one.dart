679773c4bdc05ebed5ecea86caf2d7c0a423ad60
Mahafujmr <mrtuhin444@gmail.com>
2025-08-02 17:30:07 +0530

    dart list use dynamic value

---
+++ bin/dart_data_type/list/list_example_one.dart
@@ -0,0 +1,9 @@
+// write  a programme list use dynamic value
+// like use- int, double, string, boolean
+void main(){
+  var  newValue = [10,22.33, true,'Tuhin','Toma'];
+  print(newValue);
+  // access string valu
+  print(newValue[3]);
+}
+// this list is use dynamic value
