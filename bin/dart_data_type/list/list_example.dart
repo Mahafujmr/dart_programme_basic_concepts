d2e8e500284ed285c47edad6f46da28c1986a162
Mahafujmr <mrtuhin444@gmail.com>
2025-08-02 17:25:41 +0530

    dart list

--- bin/dart_data_type/list/list_example.dart
+++ bin/dart_data_type/list/list_example.dart
@@ -2,4 +2,8 @@
 void main(){
   var city = ['Kushtia','Khulna','Varamara','Daulatpur'];
   print(city);
+  print(city[3]); // access element from list
+  // list item access -use index number
+  // index number start 0 ,1,2,3,3 .....
 }
+// list always start - []
