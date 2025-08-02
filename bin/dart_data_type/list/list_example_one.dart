33cf9cd05d3578ff7e7ec026a9926f2ce41cf613
Mahafujmr <mrtuhin444@gmail.com>
2025-08-02 20:01:19 +0800

    dart List update

--- bin/dart_data_type/list/list_example_one.dart
+++ bin/dart_data_type/list/list_example_one.dart
@@ -3,7 +3,9 @@
 void main(){
   var  newValue = [10,22.33, true,'Tuhin','Toma'];
   print(newValue);
-  // access string valu
+  // access string value
   print(newValue[3]);
+  // access boolean value
+  print(newValue[2]);
 }
 // this list is use dynamic value
