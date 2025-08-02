830f1d4a0e955bf8aa297dd676fec314220d53ac
Mahafujmr <mrtuhin444@gmail.com>
2025-08-02 17:56:42 +0530

    Dart Map

---
+++ bin/dart_data_type/map/map_example.dart
@@ -0,0 +1,13 @@
+// write a programme dart map
+/*
+map key value = key:value
+map separate , and include {}
+ */
+void main(){
+  var student = {
+    'name':'Tuhin',
+    'age': 20,
+    'school': "Pragpur High School",
+  };
+  print(student);
+}
